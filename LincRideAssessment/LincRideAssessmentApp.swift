3ba012ea944fb59ed66cced2b5b49b519b23ba3f
Ossai Michael <ossaimike8@gmail.com>
2025-08-05 19:25:15 +0100

    Initial Commit

---
+++ LincRideAssessment/LincRideAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  LincRideAssessmentApp.swift
+//  LincRideAssessment
+//
+//  Created by Michael Ossai on 05/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct LincRideAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LincRideAssessmentTests/LincRideAssessmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  LincRideAssessmentTests.swift
+//  LincRideAssessmentTests
+//
+//  Created by Michael Ossai on 05/08/2025.
+//
+
+import Testing
+@testable import LincRideAssessment
+
+struct LincRideAssessmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
